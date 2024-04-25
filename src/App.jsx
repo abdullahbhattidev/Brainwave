6a92eab24726fc548403ae6b6e752353f6209167
abdullahbhattidev <ab76293@gmail.com>
2024-04-26 04:41:29 +0500

    build a reusable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden">
+        <Button className="mt-10" href="#">
+          Button
+        </Button>
+      </div>
+      <ButtonGradient href="#" />
+    </>
+  );
 }
 
 export default App;
