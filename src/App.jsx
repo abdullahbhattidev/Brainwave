14886a44cb5740e54f400aa863fcf572b487c295
abdullahbhattidev <ab76293@gmail.com>
2024-04-26 18:53:18 +0500

    build a  responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden">
-        <Button className="mt-10" href="#">
-          Button
-        </Button>
-      </div>
+      <Header/>
       <ButtonGradient href="#" />
     </>
   );
